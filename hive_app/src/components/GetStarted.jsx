87f9b85952a129be888d64fd10e676adedf136f4
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2023-11-30 00:21:15 +0300

    Adding the Hero Section

--- hive_app/src/components/GetStarted.jsx
+++ hive_app/src/components/GetStarted.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { arrowUp } from "../assets";
+import styles from "../style";
 
-const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
-  )
-}
 
+const GetStarted = () => (
+  
+    <div className={`${styles.flexCenter} w-
+    [140px] h-[140px] rounded-full 
+    bg-blue-gradient`}>
+      
+    </div>
+  
+)
 export default GetStarted
